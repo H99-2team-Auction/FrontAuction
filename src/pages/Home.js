1e608b3fc0ad8e1ea55481ff6f0ecdde2907c2a5
Byeong Min Cho <merrybmc@gmail.com>
2022-10-22 09:55:23 +0900

    home/main background layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
+import styled from 'styled-components';
+
+const StpostContainer = styled.div`
+  margin: 0 auto;
+`;
+const StpostHeader = styled.div`
+  width: 500px;
+  height: 70px;
+  background-color: black;
+`;
+
 export default function Home() {
   return (
-    <div>
-      Home
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-      <div>zzz</div>
-    </div>
+    <>
+      <StpostContainer>
+        <StpostHeader>상품</StpostHeader>
+      </StpostContainer>
+    </>
   );
 }
